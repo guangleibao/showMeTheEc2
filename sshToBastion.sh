5d8e7be9b19a3813a80b69c7b229877f871d3662
Guanglei <toddbgl@hotmail.com>
2018-04-10 17:18:31 +0800

    comments added to sshToBastion.sh

--- sshToBastion.sh
+++ sshToBastion.sh
@@ -5,6 +5,10 @@ else
 
 # S C R I P T  B E G I N
 
+# Prerequisites
+# The bastion server is linux, and the default user is ec2-user.
+# The bastion server must have tag key *Name* and the tag value is unqiue.
+
 BASTION_NAME=${1}
 PROFILE=${2}
 SGID=`aws ec2 describe-instances --filters "Name=tag:Name,Values=${BASTION_NAME}" "Name=instance-state-name,Values=running" --query "Reservations[0].Instances[0].SecurityGroups[0].GroupId" --output text --profile ${PROFILE}`
