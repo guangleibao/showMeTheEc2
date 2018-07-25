e2ab5e1a3c2b6363a328669bfe325d5907cbf114
Guanglei <toddbgl@hotmail.com>
2018-07-25 16:23:24 +0800

    minor update

--- showInstanceOS.sh
+++ showInstanceOS.sh
@@ -1,8 +1,8 @@
 PROFILE=${1}
-echo
+echo Name, Image
 for i in `aws ec2 describe-instances --query "Reservations[*].Instances[*].InstanceId" --filter "Name=instance-state-name,Values=running" --profile ${PROFILE} --output text`; do
 	IMAGE_ID=`aws ec2 describe-instances --instance-ids ${i} --query "Reservations[0].Instances[0].ImageId" --profile ${PROFILE} --output text`
 	IMAGE_DESC=`aws ec2 describe-images --image-ids ${IMAGE_ID} --query "Images[0].Name" --profile ${PROFILE} --output text`
-	echo INSTANCE_ID:[${i}], IMAGE:[${IMAGE_DESC}]
+	echo ${i}, ${IMAGE_DESC}
 done;
 echo
